da8c8658f313084fc83de0d49cc62ade540b58c3
Andrei Calinescu <andrei@lunit.io>
2022-05-02 11:33:23 +0900

    feat: add resolver for 'track' query

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,6 +4,10 @@ const resolvers = {
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
+    // get a single track by id, for the Track page
+    track: (_, { id }, { dataSources }) => {
+      return dataSources.trackAPI.getTrack(id);
+    }
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
